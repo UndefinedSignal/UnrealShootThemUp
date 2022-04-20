8ff3304b5050684fde3daafd1cd8395ecdb22ebe
TestUnit <nod33@inbox.ru>
2022-04-20 23:46:11 +0700

    Hide player mesh on camera collision cases

--- Source/ShootThemUp/Public/STUPlayerCharacter.h
+++ Source/ShootThemUp/Public/STUPlayerCharacter.h
@@ -11,6 +11,7 @@
  */
 class UCameraComponent;
 class USpringArmComponent;
+class USphereComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
@@ -27,7 +28,11 @@ class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components");
 	UCameraComponent *CameraComponent;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components");
+	USphereComponent *CameraCollisionComponent;
+
 	virtual void OnDeath() override;
+	virtual void BeginPlay() override;
 
   public:
 	// Called to bind functionality to input
@@ -44,4 +49,15 @@ class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
 
 	void OnStartRunning();
 	void OnStoptRunning();
+
+	UFUNCTION()
+	void OnCameraCollisionBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+									   UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+									   const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnCameraCollisionEndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+									 UPrimitiveComponent *OtherComp, int32 OtherBodyIndex);
+
+	void CheckCameraOverlap();
 };
